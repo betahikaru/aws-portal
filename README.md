41e408237b1dd7c746714a800e8a9275e98b2ecc
betahikaru <beta.hikaru2000+bitbucket@gmail.com>
2014-05-11 04:49:31 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+aws-portal
+=========
+
+describe
+
+## Require
+* Ruby 2.0
+
+## Usage
+git clone git@github.com:betahikaru/aws-tools.git
+bundle install --path vendor/bundle
+bundle exec rackup
---
+++ app.rb
@@ -0,0 +1,52 @@
+require 'time'
+require 'sinatra'
+require 'sinatra/base'
+require 'sinatra/reloader'
+require 'aws-sdk-core'
+
+class AwsPortal < Sinatra::Base
+
+	before do
+	end
+
+	after do
+	end
+
+	get '/' do
+		@navbar_buttun_active = "#navbar_button_home"
+		erb :index
+	end
+
+	get '/ec2/summary' do
+		ec2 = Aws::EC2.new
+		@entities = []
+		resp = ec2.describe_instances(max_results: 10)
+		resp.reservations.each do |reservation|
+			reservation.instances.each do |instance|
+				instance_name = ""
+				instance.tags.each do |tag|
+					if instance_name = "" then
+						instance_name = tag.value
+					end
+				end
+				entity = {
+					:name => instance_name,
+					:status => instance.state.name,
+					:dns_name => instance.public_dns_name,
+					:public_ip_address => instance.public_ip_address,
+					:private_ip_address => instance.private_ip_address,
+					:id => instance.instance_id
+				}
+				@entities.push entity
+			end
+		end
+
+		@navbar_buttun_active = "#navbar_button_ec2_summary"
+		erb :"ec2/summary"
+	end
+
+	get '/time' do
+		time = Time.now
+		'{"status":"success","time":"' + "#{time.iso8601}" + '"}'
+	end
+end
