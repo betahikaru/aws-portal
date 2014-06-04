15ee1f2fdffd6826dd40c196246ed0e8a1497086
betahikaru <beta.hikaru2000+bitbucket@gmail.com>
2014-06-05 02:13:54 +0900

    Move function from AwsPortal::Applicatoin class to module AwsPortal::EC2.

--- lib/aws_portal.rb
+++ lib/aws_portal.rb
@@ -5,6 +5,8 @@ require 'sinatra/reloader'
 require 'sinatra/contrib'
 require 'aws-sdk-core'
 
+require_relative 'aws_portal/ec2'
+
 module AwsPortal
 class Application < Sinatra::Base
 
@@ -14,6 +16,9 @@ class Application < Sinatra::Base
 	# setting for directory path
 	set :root, File.join(File.dirname(__FILE__), "..")
 	set :views, Proc.new { File.join(root, "views") }
+
+	# include module
+	include AwsPortal::Ec2
 
 	get '/' do
 		@navbar_button_active = "#navbar_button_home"
@@ -97,97 +102,5 @@ class Application < Sinatra::Base
 			erb :"ec2/control/stop"
 		end
 	end
-
-	def stop_ec2_instance(dry_run, instance_id)
-		ec2 = Aws::EC2.new
-		instances = []
-		begin
-			responce = ec2.stop_instances(
-				dry_run: dry_run,
-				instance_ids: [instance_id],
-				force: false
-			)
-		rescue => exp
-			p exp
-			raise exp
-		else
-			unless responce.nil? then
-				responce.stopping_instances.each do |instance|
-					instances.push(instance)
-				end
-			end
-			return instances
-		end
-	end
-
-	def get_ec2_instances()
-		ec2 = Aws::EC2.new
-		instances = []
-		begin
-			responce = ec2.describe_instances()
-		rescue => exp
-			p exp
-			raise exp
-		else
-			unless responce.nil? then
-				responce.reservations.each do |reservation|
-					reservation.instances.each do |instance|
-						instances.push(instance)
-					end
-				end
-			end
-			return instances
-		end
-	end
-
-	def get_ec2_elasticips()
-		ec2 = Aws::EC2.new
-		begin
-			responce = ec2.describe_addresses()
-		rescue => exp
-			p exp
-			raise exp
-		else
-			return responce
-		end
-	end
-
-	def generate_instance_entity(instances)
-		entities = []
-		instances.each do |instance|
-			instance_name = ""
-			instance.tags.each do |tag|
-				if tag.key == "Name" then
-					instance_name = tag.value
-				end
-			end
-			entity = {
-				:name => instance_name,
-				:status => instance.state.name,
-				:dns_name => instance.public_dns_name,
-				:public_ip_address => instance.public_ip_address,
-				:private_ip_address => instance.private_ip_address,
-				:id => instance.instance_id
-			}
-			entities.push entity
-		end
-		return entities
-	end
-
-	def generate_eip_entities(eips)
-		eipEntities = []
-		eips.addresses.each do |address|
-			eipEntity = {
-				:public_ip_address => address.public_ip,
-				:instance_id => address.instance_id,
-				:domain => address.domain,
-				:allocation_id => address.allocation_id,
-				:association_id => address.association_id
-			}
-			eipEntities.push(eipEntity)
-		end
-		return eipEntities
-	end
-
 end
 end
