56e333532dc11bc573bf222056ca6963ee1ad825
betahikaru <beta.hikaru2000+bitbucket@gmail.com>
2014-06-04 23:32:45 +0900

    Add spec. /ec2/summary and /ec2/control return 200 OK.

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -2,17 +2,21 @@
 ENV['RACK_ENV'] = 'test'
 require File.join(File.dirname(__FILE__), 'spec_helper.rb')
 
-describe "App" do
+describe "AppPortal" do
   include Rack::Test::Methods
 
   def app
     @app ||= AwsPortal
   end
 
-  describe "request" do
-    describe "top page" do
+  %w{
+    /
+    /ec2/summary
+    /ec2/control
+    }.each do |uri|
+    describe "'#{uri}' page" do
       it "return 200 OK" do
-        get '/'
+        get uri
         expect(last_response).to be_ok
       end
     end
