b2c05867de42a72dac4fea293e7af6b5715d5213
betahikaru <beta.hikaru2000+bitbucket@gmail.com>
2014-06-05 23:58:38 +0900

    Move path of aws_portal_spec.rb from spec/aws_portal/ to spec/

--- spec/aws_portal_spec.rb
+++ spec/aws_portal_spec.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 ENV['RACK_ENV'] = 'test'
-require File.join(File.dirname(__FILE__), '..', 'spec_helper.rb')
+require File.join(File.dirname(__FILE__), 'spec_helper.rb')
 
 describe "AppPortal" do
   include Rack::Test::Methods
