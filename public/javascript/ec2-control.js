8bae58677dc1608f675190770c1f69fa67bec909
betahikaru <beta.hikaru2000+github@gmail.com>
2014-06-22 02:20:19 +0900

    Add button for starting instance on /ec2/control page

--- public/javascript/ec2-control.js
+++ public/javascript/ec2-control.js
@@ -1,12 +1,20 @@
 
 jQuery(function($) {
-	$('[data-btn-ec2-action="stop-instance"]').each(function() {
-		$(this).click(function() {
-			var instanceId = $(this).data('ec2InstanceId');
-			var url = "/ec2/control/stop/" + instanceId;
-			location.href = url;
-		});
-	});
+  $('[data-btn-ec2-action="stop-instance"]').each(function() {
+    $(this).click(function() {
+      var instanceId = $(this).data('ec2InstanceId');
+      var url = "/ec2/control/stop/" + instanceId;
+      location.href = url;
+    });
+  });
+
+  $('[data-btn-ec2-action="start-instance"]').each(function() {
+    $(this).click(function() {
+      var instanceId = $(this).data('ec2InstanceId');
+      var url = "/ec2/control/start/" + instanceId;
+      location.href = url;
+    });
+  });
 
 });
 
